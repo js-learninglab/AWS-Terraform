c87a2de609cc3480c55ff8c85213c8214b815686
Js-learninglab <js.learninglab@hotmail.com>
2025-09-05 15:44:50 +0800

    updating the output file

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "instance_hostname" {
   description = "Private DNS name of the EC2 instance."
   value       = aws_instance.app_server.private_dns
 }
+
+output "db_server_private_dns" {
+  value = aws_instance.db_server.private_dns
+}
