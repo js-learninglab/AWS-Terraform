ada6bd3abc9da797ab0488fe935cc2459a1efd67
Js-learninglab <js.learninglab@hotmail.com>
2025-09-11 16:43:56 +0800

    configured google auth

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "app_server_public_dns" {
+output "app_server_private_dns" {
   description = "Private DNS name of the EC2 instance."
   value       = [for instance in aws_instance.app_server : instance.private_dns]
 }
--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,7 @@ terraform {
             source = "hashicorp/aws"
             version = "~> 5.92"
         }
-        gcp = {
+        google = {
             source = "hashicorp/google"
             version = "~> 6.0"
         }
